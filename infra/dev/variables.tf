a8a6acdd97f84ee1b41da0be704867eaa6c9be99
Pradipta Sanyal(Prad) <pradipta.sanyal@generac.com>
2021-10-28 13:11:02 -0700

    variables updated

--- infra/dev/variables.tf
+++ infra/dev/variables.tf
@@ -1,7 +1,23 @@
-variable "ecs_cluster_name" {}
+variable "ecs_cluster_name" {
+  description = "Name of ECS Cluster"
+  type        = string
+  default     = "test-ecs-cluster"
+}
 
-variable "capacity_providers" {}
+variable "capacity_providers" {
+  description = "Capacity Providers"
+  type        = list(string)
+  default     = []
+}
 
-variable "default_capacity_provider_strategy" {}
+variable "default_capacity_provider_strategy" {
+  description = "Capacity Providers Strategy"
+  type        = list(map(any))
+  default     = []
+}
 
-variable "cluster_tags" {}
+variable "cluster_tags" {
+  description = "Tags"
+  type        = map(string)
+  default     = {}
+}
