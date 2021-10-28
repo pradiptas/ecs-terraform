9dd36efb11fb58ac20db3fb48842c5a73ea03bab
Pradipta Sanyal(Prad) <pradipta.sanyal@generac.com>
2021-10-28 12:59:00 -0700

    tfvars introduced

---
+++ infra/dev/variables.tfvars
@@ -0,0 +1,4 @@
+ecs_cluster_name = "pradipta-ecs-cluster-terraform-01-dev"
+capacity_providers = ""
+default_capacity_provider_strategy = ""
+cluster_tags = ""
--- infra/production/variables.tf
+++ infra/production/variables.tf
@@ -1,7 +1,7 @@
 variable "ecs_cluster_name" {
   description = "Name of ECS Cluster"
   type        = string
-  default     = "pradipta-ecs-cluster-terraform-01-dev"
+  default     = "pradipta-ecs-cluster-terraform-01-prod"
 }
 
 variable "capacity_providers" {
